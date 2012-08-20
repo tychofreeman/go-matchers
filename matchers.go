38530a11c1772458b3761bb976996a888045696d
Christopher Freeman <cwfreeman@gmail.com>
2012-08-19 23:06:40 -0500

    Added HasMessage, IsTrue, and IsFalse matchers/generators.

--- matchers.go
+++ matchers.go
@@ -1,5 +1,10 @@
 package matchers
 
+import (
+    "fmt"
+    "reflect"
+)
+
 type Errorable interface {
     Errorf(string, ...interface{})
 }
@@ -11,3 +16,35 @@ func AssertThat(t Errorable, expected interface{}, m Matcher) {
         t.Errorf(msg)
     }
 }
+
+func Not(m Matcher) Matcher {
+    return func(actual interface{}) (bool, string) {
+        ok, msg := m(actual)
+        return !ok, "not " + msg
+    }
+}
+
+func isBool(expected bool, actual interface{}) (bool, string) {
+    switch a := actual.(type) {
+        case bool:
+            return a == expected, fmt.Sprintf("'%v' was expected, but got %v", expected, a)
+    }
+    return false, fmt.Sprintf("'%v' was expected, but got non-boolean of type %s", expected, reflect.TypeOf(actual).Name())
+}
+
+func IsTrue(actual interface{}) (bool, string) {
+    return isBool(true, actual)
+}
+
+func IsFalse(actual interface{}) (bool, string) {
+    return isBool(false, actual)
+}
+
+func HasMessage(expected string, m Matcher) Matcher {
+    return func (underTest interface{}) (bool, string) {
+        _, actual := m(underTest)
+        return expected == actual, fmt.Sprintf("'%v' expected, but got '%v'", expected, actual)
+    }
+}
+
+func 
--- matchers_test.go
+++ matchers_test.go
@@ -4,6 +4,8 @@ import (
     "testing"
 )
 
+
+// Mock part of testing.T
 type MockT struct {
     errArgs []string
 }
@@ -12,11 +14,20 @@ func (mock *MockT) Errorf(format string, args ...interface{}) {
     mock.errArgs = append(mock.errArgs, format)
 }
 
+// Document that we don't care about this parameter.
+type Ignored struct {
+}
+
+// Create a Matcher function with the given return values
+func ConstMatcher(matches bool, msg string) Matcher {
+    return func(actual interface{}) (bool, string) {
+        return matches, msg
+    }
+}
+
 func TestAssertThatLogsStringWhenMatchFails(t *testing.T) {
     var mockT MockT
-    AssertThat(&mockT, true, func(actual interface{}) (bool, string) {
-        return false, "explanation"
-    })
+    AssertThat(&mockT, Ignored{}, ConstMatcher(false, "explanation"))
 
     if mockT.errArgs[0] != "explanation" {
         t.Errorf("Expected that matchers.AssertThat() would pass 'explanation', but it received '%v'", mockT.errArgs)
@@ -25,11 +36,49 @@ func TestAssertThatLogsStringWhenMatchFails(t *testing.T) {
 
 func TestAssertThatLogsNothingWhenMatchSucceeds(t *testing.T) {
     var mockT MockT
-    AssertThat(&mockT, true, func(actual interface{}) (bool, string) {
-        return true, "explanation"
-    })
+    AssertThat(&mockT, Ignored{}, ConstMatcher(true, "explanation"))
 
     if len(mockT.errArgs) > 0 {
         t.Errorf("Expected that matchers.AssertThat() would not fail when matching passes, but it did.")
     }
+}
+
+func TestNotInvertsFalseMatcher(t *testing.T) {
+    AssertThat(t, Ignored{}, Not(ConstMatcher(false, "")))
+}
+
+func TestNotNotKeepsTrueMatcher(t *testing.T) {
+    AssertThat(t, Ignored{}, Not(Not(ConstMatcher(true, ""))))
+}
+
+func TestNotAddsNotToMessage(t *testing.T) {
+    var mockT MockT
+    AssertThat(&mockT, Ignored{}, Not(ConstMatcher(true, "bob")))
+    if mockT.errArgs[0] != "not bob" {
+        t.Errorf("Expected Not() to add 'not ' to error message, but the actual message was '%v'", mockT.errArgs)
+    }
+}
+
+func TestFalseIsTrueIsFalse(t *testing.T) {
+    AssertThat(t, false, Not(IsTrue))
+}
+
+func TestTrusIsTrueIsTrue(t *testing.T) {
+    AssertThat(t, true, IsTrue)
+}
+
+func TestFalseIsFalseIsTrue(t *testing.T) {
+    AssertThat(t, false, IsFalse)
+}
+
+func TestTrusIsFalseIsFalse(t *testing.T) {
+    AssertThat(t, true, Not(IsFalse))
+}
+
+func TestRecognizesConstantMessage(t *testing.T) {
+    AssertThat(t, Ignored{}, HasMessage("test message", ConstMatcher(false, "test message")))
+}
+
+func TestFailsIfMessageIsNotGiven(t *testing.T) {
+    AssertThat(t, Ignored{}, Not(HasMessage("not found", ConstMatcher(false, "this is the real msg"))))
 }
